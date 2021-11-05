d887caf27ecd37e2004a804cb7c7169b1f320ae2
bucknermi <michaeliwbuckner@gmail.com>
2021-11-05 16:26:42 -0500

    Creating initial Spring project
    Creating spring project basic configuration

---
+++ Synergy-Project2/src/main/java/com/revature/SynergyProject2Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyProject2Application.class, args);
+	}
+
+}
