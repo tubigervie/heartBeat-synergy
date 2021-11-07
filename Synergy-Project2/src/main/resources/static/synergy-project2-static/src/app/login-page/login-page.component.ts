1540a3585b221c003440597fd8a4b8e3693d3ea4
bucknermi <michaeliwbuckner@gmail.com>
2021-11-07 12:26:32 -0600

    Updated outputs of getSong()
    getSong() now displays the song image, artist name, album name and song name

--- Synergy-Project2/src/main/resources/static/synergy-project2-static/src/app/login-page/login-page.component.ts
+++ Synergy-Project2/src/main/resources/static/synergy-project2-static/src/app/login-page/login-page.component.ts
@@ -12,9 +12,14 @@ export class LoginPageComponent implements OnInit {
   public token:string = '';
   public newReleases:string = '';
   public song:string = '';
+  public songUrl:string = '';
   public songResult:string = '';
   public songId:string = '';
-  public songResultArray: any[] = [];
+  public artistName:string = '';
+  public albumName:string = '';
+  public songName:string = '';
+  public albumImageUrl:string = '';
+ 
 
   constructor(private accountService:AccountService) { }
 
@@ -42,22 +47,14 @@ export class LoginPageComponent implements OnInit {
    searchSong() {
      this.accountService.searchSongServ(this.token, this.song).subscribe(
       (data:Object)=> {
-        this.songResultArray = Object.entries(data); 
-        console.log(Object.keys(data));
-        console.log(Object.values(data));
         let innerData:any[]=Object.values(data);
-        console.log(innerData);
         let innerInfo:any[]=Object.values(innerData[0]);
-        console.log(innerInfo);
         let innerSongs:any[]=Object.values(innerInfo[1]);
-        console.log(innerSongs);
         let innerSongsInfo:any[]=Object.values(innerSongs[0]);
-        console.log(innerSongsInfo);
         let innerSongsInfoUrl:any[]=Object.values(innerSongsInfo[6]);
-        console.log(innerSongsInfoUrl);
         let finalUrl = innerSongsInfoUrl[0];
-        console.log(finalUrl);
-        this.songResult = (finalUrl);
+        this.songId= finalUrl.substring(31, finalUrl.length);
+        this.songUrl =finalUrl;
        
       }
      )
@@ -72,7 +69,30 @@ export class LoginPageComponent implements OnInit {
      this.accountService.getSongServ(this.token, this.songId).subscribe(
       (data:Object)=> {
         this.songResult = JSON.stringify(data);
-        console.log(this.songResult);
+        let innerData = Object.values(data);
+        console.log(innerData);
+        console.log(JSON.stringify(innerData));
+        let innerArtistandAlbum:any[] = Object.values(innerData[0]);
+        console.log(innerArtistandAlbum);
+        let innerArtistInfo:any[] = Object.values(innerArtistandAlbum[1]);
+        console.log(innerArtistInfo);
+        let innerArtistDetails:any[] = Object.values(innerArtistInfo[0]);
+        console.log(innerArtistDetails);
+        let artistName = innerArtistDetails[3];
+        console.log(artistName);
+        this.artistName = artistName;
+        let albumName = innerArtistandAlbum[6];
+        this.albumName = albumName;
+        let songName = innerData[11];
+        this.songName = songName;
+        let innerAlbumImageInfo:any[] = Object.values(innerArtistandAlbum[5]);
+        console.log(innerAlbumImageInfo);
+        let innerAlbumImageDetails:any[] = Object.values(innerAlbumImageInfo[0]);
+        console.log(innerAlbumImageDetails);
+        let albumImageUrl = innerAlbumImageDetails[1];
+        console.log(albumImageUrl);
+        this.albumImageUrl = albumImageUrl;
+
       }
      )
    }
