3336a2709f6acbf5adca78e464d28b567286b5de
tubigervie <etubig@uci.edu>
2021-11-08 22:31:29 -0800

    User account, genre, and artist table generated, able to send post, get, delete requests for user accounts and user top artists

---
+++ Synergy-Project2/src/main/java/com/revature/repos/HBTopArtistDAO.java
@@ -0,0 +1,13 @@
+package com.revature.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revature.models.HBTopArtist;
+import com.revature.models.HBUserAccount;
+
+public interface HBTopArtistDAO extends JpaRepository<HBTopArtist, Integer>{
+
+	List<HBTopArtist> findByUser(HBUserAccount user);
+}
