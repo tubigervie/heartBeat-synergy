7d12c0b297783ba061dd108091b3f5c3c1df0a0d
bucknermi <michaeliwbuckner@gmail.com>
2021-11-16 20:10:12 -0600

    Update SynergyProject2ApplicationTests.java

--- src/test/java/com/revature/SynergyProject2ApplicationTests.java
+++ src/test/java/com/revature/SynergyProject2ApplicationTests.java
@@ -24,6 +24,7 @@ import org.springframework.http.ResponseEntity;
 
 
 
+
 import com.revature.controllers.HBLoginController;
 import com.revature.models.HBLoginDTO;
 import com.revature.models.HBMatch;
